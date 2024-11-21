26946263e80400426269a36983978217a3421f5d
manasapal96 <manasa.palaparthy96@gmail.com>
2024-11-21 20:30:11 +0530

    Adding first java program

---
+++ Remove Special characters.java
@@ -0,0 +1,9 @@
+public class RemoveSpecialCharacters{
+
+     public static void main(String []args){
+        String s= "*&^%$12gh67kj34cu(*& _";
+		//Using regular expression replace all method
+        s= s.replaceAll("[^a-zA-Z0-9]", "");
+        System.out.println(s);
+     }
+}
