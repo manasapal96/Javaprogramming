a974ba3d1919d1114daece4ff42b190f29e33da6
manasapal96 <manasa.palaparthy96@gmail.com>
2024-12-08 18:03:54 +0530

    Update Remove Special characters.java

--- Remove Special characters.java
+++ Remove Special characters.java
@@ -2,7 +2,7 @@ public class RemoveSpecialCharacters{
 
      public static void main(String []args){
         String s= "*&^%$12gh67kj34cu(*& _";
-		//Using regular expression replace all method
+		//Using regular expression replace all methods
         s= s.replaceAll("[^a-zA-Z0-9]", "");
         System.out.println(s);
      }
